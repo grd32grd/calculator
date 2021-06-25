596eea2becf3d0627a6a85e4526d143140478139
guledmoh12 <guledmo12@gmail.com>
2021-06-25 13:43:31 -0400

    Match CSS styling to look like the iOS calculator one

--- src/calculatorView.java
+++ src/calculatorView.java
@@ -1,11 +1,13 @@
 //Imports
 import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.*;
+
 
 
 //View Class
@@ -16,13 +18,17 @@ public class calculatorView extends Pane {
     Font resultFont = Font.font("Calibri", FontWeight.EXTRA_BOLD, 22);
     Font buttonFont = Font.font("Calibri", FontWeight.BOLD, 16);
 
-    String fontStyle =  "-fx-text-fill: white;";
-    String backStyle = "-fx-background-color: black;";
+    //String fontStyle =  "-fx-text-fill: white;";
+    //String backStyle = "-fx-background-color: black;";
+
+    String numberStyle = "-fx-text-fill: #FFFFFF; -fx-background-color: #333333";
+    String operationStyle = "-fx-text-fill: #FFFFFF; -fx-background-color: #F2A33C";
+    String functionStyle = "-fx-text-fill: #000000; -fx-background-color: #A5A5A5";
+    String resultStyle = "-fx-text-fill: #FFFFFF; -fx-background-color: #000000";
 
 
 
     public void update(){
-
     }
 
     public calculatorView() {
@@ -30,121 +36,123 @@ public class calculatorView extends Pane {
         zero.setPrefSize(105,50);
         zero.relocate(5,305);
         zero.setFont(buttonFont);
-        zero.setStyle(backStyle + fontStyle);
+        zero.setStyle(numberStyle);
+        //zero.setTextAlignment(TextAlignment.JUSTIFY);
 
         period = new Button(".");
         period.setPrefSize(50,50);
         period.relocate(115,305);
         period.setFont(buttonFont);
-        period.setStyle(backStyle + fontStyle);
+        period.setStyle(numberStyle);
 
         equals = new Button("=");
         equals.setPrefSize(50,50);
         equals.relocate(170,305);
         equals.setFont(buttonFont);
-        equals.setStyle(backStyle + fontStyle);
+        equals.setStyle(operationStyle);
 
         one = new Button("1");
         one.setPrefSize(50,50);
         one.relocate(5,250);
         one.setFont(buttonFont);
-        one.setStyle(backStyle + fontStyle);
+        one.setStyle(numberStyle);
 
         two = new Button("2");
         two.setPrefSize(50,50);
         two.relocate(60,250);
         two.setFont(buttonFont);
-        two.setStyle(backStyle + fontStyle);
+        two.setStyle(numberStyle);
 
         three = new Button("3");
         three.setPrefSize(50,50);
         three.relocate(115,250);
         three.setFont(buttonFont);
-        three.setStyle(backStyle + fontStyle);
+        three.setStyle(numberStyle);
 
         add = new Button("+");
         add.setPrefSize(50,50);
         add.relocate(170,250);
         add.setFont(buttonFont);
-        add.setStyle(backStyle + fontStyle);
+        add.setStyle(operationStyle);
 
         four = new Button("4");
         four.setPrefSize(50,50);
         four.relocate(5,195);
         four.setFont(buttonFont);
-        four.setStyle(backStyle + fontStyle);
+        four.setStyle(numberStyle);
 
         five = new Button("5");
         five.setPrefSize(50,50);
         five.relocate(60,195);
         five.setFont(buttonFont);
-        five.setStyle(backStyle + fontStyle);
+        five.setStyle(numberStyle);
 
         six = new Button("6");
         six.setPrefSize(50,50);
         six.relocate(115,195);
         six.setFont(buttonFont);
-        six.setStyle(backStyle + fontStyle);
+        six.setStyle(numberStyle);
 
         subtract = new Button("-");
         subtract.setPrefSize(50,50);
         subtract.relocate(170,195);
         subtract.setFont(buttonFont);
-        subtract.setStyle(backStyle + fontStyle);
+        subtract.setStyle(operationStyle);
 
         seven = new Button("7");
         seven.setPrefSize(50,50);
         seven.relocate(5,140);
         seven.setFont(buttonFont);
-        seven.setStyle(backStyle + fontStyle);
+        seven.setStyle(numberStyle);
 
         eight = new Button("8");
         eight.setPrefSize(50,50);
         eight.relocate(60,140);
         eight.setFont(buttonFont);
-        eight.setStyle(backStyle + fontStyle);
+        eight.setStyle(numberStyle);
 
         nine = new Button("9");
         nine.setPrefSize(50,50);
         nine.relocate(115,140);
         nine.setFont(buttonFont);
-        nine.setStyle(backStyle + fontStyle);
+        nine.setStyle(numberStyle);
 
         multiply = new Button("x");
         multiply.setPrefSize(50,50);
         multiply.relocate(170,140);
         multiply.setFont(buttonFont);
-        multiply.setStyle(backStyle + fontStyle);
+        multiply.setStyle(operationStyle);
 
         clear = new Button("C");
         clear.setPrefSize(50,50);
         clear.relocate(5,85);
         clear.setFont(buttonFont);
-        clear.setStyle(backStyle + fontStyle);
+        clear.setStyle(functionStyle);
 
         sign = new Button("+/-");
         sign.setPrefSize(50,50);
         sign.relocate(60,85);
         sign.setFont(buttonFont);
-        sign.setStyle(backStyle + fontStyle);
+        sign.setStyle(functionStyle);
 
         percent = new Button("%");
         percent.setPrefSize(50,50);
         percent.relocate(115,85);
         percent.setFont(buttonFont);
-        percent.setStyle(backStyle + fontStyle);
+        percent.setStyle(functionStyle);
 
         divide = new Button("÷");
         divide.setPrefSize(50,50);
         divide.relocate(170,85);
         divide.setFont(buttonFont);
-        divide.setStyle(backStyle + fontStyle);
+        divide.setStyle(operationStyle);
 
         result = new TextField();
         result.setPrefSize(215,75);
         result.relocate(5,5);
         result.setFont(resultFont);
-        result.setStyle(backStyle + fontStyle);
+        result.setStyle(resultStyle);
+        result.setAlignment(Pos.BASELINE_RIGHT);
 
         getChildren().addAll(zero, period, equals, one, two, three, add, four, five, six, subtract, seven, eight, nine, multiply, clear, sign, percent, divide, result);
         setPrefSize(225, 360);
